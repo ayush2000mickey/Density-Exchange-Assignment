673d7c8ab3e985e6eabae61dd92df312c468dfbc
Ayush Kumar <ayush2000mickey@gmail.com>
2023-07-15 15:27:18 +0530

    added hover effect on cards

--- src/app/components/DoesThisSoundsFamiliar/DoesThisSoundsFamiliar.tsx
+++ src/app/components/DoesThisSoundsFamiliar/DoesThisSoundsFamiliar.tsx
@@ -43,10 +43,10 @@ const DoesThisSoundsFamiliar = () => {
         <h1 className="text-2xl font-bold px-4">Does this sound familiar...</h1>
       </motion.div>
       <div
-        className=" mt-10 flex gap-7 items-center overflow-x-auto hide-scrollbar"
+        className=" p-10 flex space-x-8 gap-7 items-center overflow-x-auto hide-scrollbar h-auto"
         ref={boxContainerRef}
       >
-        <div className=" ease-linear rounded-xl bg-purple-200 w-[350px]  p-6 flex flex-col gap-4 shrink-0">
+        <div className=" ease-linear rounded-xl bg-purple-200 min-w-[350px]  p-6 flex flex-col gap-4 hover:scale-110 hover:-rotate-12 hover:duration-200">
           <Image width={70} height={70} alt="" src="/angry.png" />
           <h3 className="font-bold text-lg">You argue with a colleague</h3>
           <p className="text-gray-700">
@@ -55,7 +55,7 @@ const DoesThisSoundsFamiliar = () => {
           </p>
         </div>
 
-        <div className="rounded-xl bg-blue-300 w-[350px]  p-6 flex flex-col gap-4 shrink-0">
+        <div className="rounded-xl bg-blue-300 min-w-[350px]  p-6 flex flex-col gap-4 hover:scale-110 hover:-rotate-12 hover:duration-200">
           <Image width={70} height={70} alt="" src="/happy.png" />
           <h3 className="font-bold text-lg text-white">
             You get a prmotion at work
@@ -66,7 +66,7 @@ const DoesThisSoundsFamiliar = () => {
           </p>
         </div>
 
-        <div className="rounded-xl bg-yellow-200 w-[350px]  p-6 flex flex-col gap-4 shrink-0">
+        <div className="rounded-xl bg-yellow-200 min-w-[350px]  p-6 flex flex-col gap-4 hover:scale-110 hover:-rotate-12 hover:duration-200">
           <Image width={70} height={70} alt="" src="/shocked.png" />
           <h3 className="font-bold text-lg ">You attend a class reunion</h3>
           <p className="text-gray-700">
@@ -75,7 +75,7 @@ const DoesThisSoundsFamiliar = () => {
           </p>
         </div>
 
-        <div className=" rounded-xl bg-violet-600 w-[350px]  p-6 flex flex-col gap-4 shrink-0">
+        <div className=" rounded-xl bg-violet-600 min-w-[350px]  p-6 flex flex-col gap-4 hover:scale-110 hover:-rotate-12 hover:duration-200">
           <Image width={70} height={70} alt="" src="/wow.png" />
           <h3 className="font-bold text-lg ">
             You are at a lively dinner party
@@ -86,7 +86,7 @@ const DoesThisSoundsFamiliar = () => {
           </p>
         </div>
 
-        <div className="rounded-xl bg-blue-100 w-[350px]  p-6 flex flex-col gap-4 shrink-0">
+        <div className="rounded-xl bg-blue-100 min-w-[350px]  p-6 flex flex-col gap-4 hover:scale-110 hover:-rotate-12 hover:duration-200">
           <Image width={70} height={70} alt="" src="/sad.png" />
           <h3 className="font-bold text-lg ">
             You hit dead end in a negotiation
@@ -97,7 +97,7 @@ const DoesThisSoundsFamiliar = () => {
           </p>
         </div>
 
-        <div className="rounded-xl bg-purple-200 w-[350px]  p-6 flex flex-col gap-4 shrink-0">
+        <div className="rounded-xl bg-purple-200 min-w-[350px]  p-6 flex flex-col gap-4 hover:scale-110 hover:-rotate-12 hover:duration-200">
           <Image width={70} height={70} alt="" src="/angry.png" />
           <h3 className="font-bold text-lg">You argue with a colleague</h3>
           <p className="text-gray-700">
@@ -105,7 +105,7 @@ const DoesThisSoundsFamiliar = () => {
             towards common ground
           </p>
         </div>
-        <div className=" rounded-xl bg-violet-600 w-[350px]  p-6 flex flex-col gap-4 shrink-0">
+        <div className=" rounded-xl bg-violet-600 min-w-[350px]  p-6 flex flex-col gap-4 hover:scale-110 hover:-rotate-12 hover:duration-200">
           <Image width={70} height={70} alt="" src="/wow.png" />
           <h3 className="font-bold text-lg ">
             You are at a lively dinner party
@@ -116,7 +116,7 @@ const DoesThisSoundsFamiliar = () => {
           </p>
         </div>
 
-        <div className="rounded-xl bg-blue-100 w-[350px]  p-6 flex flex-col gap-4 shrink-0">
+        <div className="rounded-xl bg-blue-100 min-w-[350px]  p-6 flex flex-col gap-4 hover:scale-110 hover:-rotate-12 hover:duration-200">
           <Image width={70} height={70} alt="" src="/sad.png" />
           <h3 className="font-bold text-lg ">
             You hit dead end in a negotiation
